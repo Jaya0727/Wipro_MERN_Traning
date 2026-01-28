aa3a35a87dd61f919716382dcae327cd61352278
Jaya0727 <146980434+Jaya0727@users.noreply.github.com>
2026-01-28 16:42:11 +0530

    Delete Phase4 react activity directory

--- Phase4 react activity/src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="bg-gray-900 text-white text-center py-6 mt-10">
-      Thanks for Shopping with Eflyer!
-    </footer>
-  );
-};
-export default Footer;
--- Phase4 react activity/src/flux/constants/ActionTypes.js
+++
@@ -1,4 +0,0 @@
-export const ADD_TO_CART = "ADD_TO_CART";
-export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
-export const SET_CART = "SET_CART";
-export const DECREASE_QTY = "DECREASE_QTY";
